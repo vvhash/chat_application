600b0545db4f07cb476701706f89d30eb58f0ec9
vivermit <vivermit@gmail.com>
2024-06-10 23:04:09 +1000

    updated socket address

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 	useEffect(() => {
 		if (authUser) {
 			
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://chat-application-hac3.onrender.com:5000", {
 				query: {
 					userId: authUser._id,
 				},
